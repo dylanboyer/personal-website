91772a1703eec784e842f57b50cce7ee57fc9d76
dylanboyer <boyer51@purdue.edu>
2023-05-14 18:10:09 -0400

    changed layout. added more links and an image. changed title and favicon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my website!</h1>
-      <h2>
-        <a
-          href="https://drive.google.com/file/d/1F0tZv4nptVm5OuBKKmWgHvr-r6Qo7MSG/view?usp=sharing"
-          target="_blank"
-        >
-          Resume
-        </a>
-      </h2>
+    <div className="wrapper">
+      <div className="App">
+        <h1>
+          <em>DYLAN BOYER</em>
+        </h1>
+        <br></br>
+        <img src="https://i.imgur.com/d2a4F3Q.jpg" width="250px"></img>
+        <br></br>
+        <p>
+          <a
+            href="https://drive.google.com/file/d/1F0tZv4nptVm5OuBKKmWgHvr-r6Qo7MSG/view?usp=sharing"
+            target="_blank"
+          >
+            resume
+          </a>
+          &nbsp;//&nbsp;
+          <a href="https://github.com/dylanboyer" target="_blank">
+            github
+          </a>
+          &nbsp;//&nbsp;
+          <a href="https://www.linkedin.com/in/dylanrboyer/" target="_blank">
+            linkedin
+          </a>
+          &nbsp;//&nbsp;
+          <a href="mailto: dylanrboyer@gmail.com" target="_blank">
+            contact
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
